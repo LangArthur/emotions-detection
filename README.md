c7c67029613621da4e1608c2808b39a6083545ce
alang <arthur.lang@epitech.eu>
2021-04-17 17:06:03 +0200

    init project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # humors-detection
 A program that can detect different humors of a person on a picture.
+
+This project has an educational purpose.
---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# Created on Sat Apr 17 2021
+#
+# Arthur Lang
+# main.py
+#
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    main()
