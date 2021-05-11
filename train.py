c6eebdd9f773b1ec2d4732fc53f07a1a22a8d20a
alang <arthur.lang@epitech.eu>
2021-05-11 15:11:39 +0200

    add train script and FER-2013 dataset

---
+++ train.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#
+# Created on Tue May 11 2021
+#
+# Arthur Lang
+# train.py
+#
+
+from src.dataset import loadFromFile
+from src.Models.ModelInception import ModelInception
+
+# @function train
+# launch a train session
+def train():
+    dataTrain, labelTrain, dataTest, labelTest = loadFromFile("ressources/datasets/FER-2013")
+    model = ModelInception()
+    model.train(dataTrain, labelTrain, (dataTest, labelTest), "ressources/weights/Inception/")
+    return 0
+
+if __name__ == '__main__':
+    train()
