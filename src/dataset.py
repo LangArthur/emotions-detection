6ea700a2c4e57da0897e32a80381168a60fadd8d
MadalinaAldea <madalinaiolanda.aldea@gmail.com>
2021-05-14 17:43:08 +0200

    function for loading from folder

--- src/dataset.py
+++ src/dataset.py
@@ -46,3 +46,46 @@ def loadFromFile(file):
     unionShuffle(dataTrain, targetTrain)
     unionShuffle(dataTest, targetTest)
     return numpy.reshape(dataTrain, (len(dataTrain), 48, 48, 1)), numpy.array(targetTrain), numpy.reshape(dataTest, (len(dataTest), 48, 48, 1)), numpy.array(targetTest)
+
+
+def load_dataset(folder):
+    data = []
+    label = []
+    images_train = []
+    labels_train = []
+    images_test = []
+    labels_test = []
+    # iterate through each folders
+    for item in os.listdir(folder):
+          datasets = os.path.join(folder, item)
+          print(datasets)
+          for element in os.listdir(datasets):
+            # print(element)
+            imagePath=os.path.join(datasets, element)
+            print(imagePath)
+            for image in os.listdir(imagePath):
+              img= cv2.imread(os.path.join(imagePath,image), cv2.IMREAD_GRAYSCALE)/255.0
+              if item=='train':
+                break
+                images_train.append(img)
+                labels_train.append((element))
+              elif item=='test':
+                images_test.append(img)
+                labels_test.append((element))
+            # iterate throught images
+            # for element in os.listdir(datasets):
+              # for digit in os.listdir()
+              #    # data.append(img)
+              #   # label.append(int(digitFolder))
+              #   # push to train tdataset
+              #   if item == "train" and img is not None:
+              #       images_train.append(img)
+              #       labels_train.append(int(number))
+              #   # push to test tdataset
+              #   elif item == "test" and img is not None:
+              #       images_test.append(img)
+              #       labels_test.append(int(number))
+    
+    # return numpy.reshape(images_train, (len(images_train), 48, 48, 1)), numpy.array(labels_train), numpy.reshape(images_test, (len(images_test), 48, 48, 1)), numpy.array(labels_test)
+    return  numpy.reshape(images_test, (len(images_test), 48, 48, 1)), numpy.array(labels_test)
+    # return data, label
