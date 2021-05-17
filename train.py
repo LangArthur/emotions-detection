98fce19a23ff1189aaeec53f79310db573168acc
MadalinaAldea <madalinaiolanda.aldea@gmail.com>
2021-05-17 13:42:21 +0000

    "reworked"

--- train.py
+++ train.py
@@ -10,11 +10,12 @@ from os import path
 import tensorflow
 import numpy
 from tensorflow import keras
-from src.dataset import load_dataset
+from src.dataset import  loadFromFile
 from src.Models.ModelInception import ModelInception
 from src.Models.SimpleCNN import SimpleCNN
-# import sklearn
-from sklearn import preprocessing
+from src import AModel
+from tensorflow import keras
+
 
 # @function trainpip install keras
 # launch a train session
@@ -27,22 +28,22 @@ def train():
 
 
 if __name__ == '__main__':
-    train()
+    # train()
     # dataTrain, labelTrain, dataTest, labelTest = load_dataset("/colabdrive/EmotionDetection/emotions-detection/ressources/datasets/EmotionDataset")
-    dataTest, labelTest = load_dataset("/colabdrive/EmotionDetection/emotions-detection/ressources/datasets/EmotionDataset")
-    labelEncoder =preprocessing.LabelEncoder()
-    labelEncoder.fit(['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral'])
-    # labelTrain1=labelEncoder.transform(labelTrain)
-    labelTest1=labelEncoder.transform(labelTest)
-    print(labelTest1)
-    print(dataTest)
+    # dataTrain, labelTrain,dataTest, labelTest = loadFromFile("/colabdrive/emotions-detection/ressources/datasets/EmotionsDataset")
     # # labelTrain1=numpy.zeros(len(labelTrain))
     # labelTest1=numpy.zeros(len(labelTest))
     # print("ok")
     # # model=SimpleCNN()
     # # model.train()
-    model=SimpleCNN()
-    model.train(dataTrain, labelTrain, (dataTest, labelTest), 150)
-    model.save("/colabdrive/EmotionDetection/emotions-detection/src/Models/SimpleCNNSaved/LoadFromFileMethod")
-
-
+    # model=AModel()
+    # model.train(dataTrain, labelTrain, (dataTest, labelTest), 100)
+    # model.save("/colabdrive/EmotionDetection/emotions-detection/src/Models/SimpleCNNSaved/LoadFromFileMethod")
+    # model=SimpleCNN()
+    # model =keras.models.load_model("/colabdrive/EmotionDetection/emotions-detection/src/Models/SimpleCNNSaved/LoadFromFileMethod")
+    # model.summary()
+    # print(model.evaluate(dataTest, labelTest))
+    converter = tensorflow.lite.TFLiteConverter.from_saved_model("/colabdrive/EmotionDetection/emotions-detection/src/Models/SimpleCNNSaved/LoadFromFileMethod") # path to the SavedModel directory
+    tflite_model = converter.convert()
+    with open('/colabdrive/emotions-detection/src/Models/model.tflite', 'wb') as f:
+        f.write(tflite_model)
