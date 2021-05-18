6ef1fbbf54b20c9bb5c1f692276f5ea720d5e297
MadalinaAldea <madalinaiolanda.aldea@gmail.com>
2021-05-19 01:12:52 +0200

    "Prediction using cnnv2"

--- src/EmotionDetector.py
+++ src/EmotionDetector.py
@@ -23,25 +23,22 @@ class EmotionDetector():
         self.isRunning = True
         self.faceClassifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
         self.emotions = ["anger","disgust","fear","happiness", "neutral", "sadness","surprise"]
-        self.model= keras.models.load_model("src\Models\LoadFromFileMethod")
+        self.model= keras.models.load_model("src\Models\Cnnv2\Cnnv2")
+        
 
     def drawFace(self, img, faces,gray):
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            gray=gray/255.0
-            gray=numpy.resize(gray,(48,48))
-            img_expanded = gray[:, :, numpy.newaxis]
-            imgForPrediction=img_expanded[numpy.newaxis,:,:,:]
-            # print(imgForPrediction.shape)
-            prediction=numpy.argmax(self.model.predict(imgForPrediction))
-            print(prediction)
+            roiGray=gray[y:y + h, x:x + w]
+            roiGray=roiGray/255.0
+            cropped_img = numpy.expand_dims(numpy.expand_dims(cv2.resize(roiGray, (48, 48)), -1), 0)
+            prediction=numpy.argmax(self.model.predict(cropped_img))
             cv2.putText(img, str(self.emotions[prediction]), (x + 6, y - 6), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 1)
 
     def run(self):
         while (self.isRunning):
             ret, frame = self._cap.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # print(gray.shape)
             faces = self.faceClassifier.detectMultiScale(gray, 1.3, 5)
             self.drawFace(frame, faces, gray)
             cv2.imshow(self.title, frame)
